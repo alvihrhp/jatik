9b20414cd9ba0e4c6717479677ba08db1129316f
Dian Gunawan <digoens@gmail.com>
2022-05-05 21:12:10 +0700

    404 page, navbar title, icons, dst done

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,10 +25,10 @@ const Layout = ({ children }) => {
           href="https://icons.duckduckgo.com/ip3/www.google.com.ico"
           sizes="16x16"
         /> */}
-        <link rel="apple-touch-icon" sizes="180x180" href="../images/apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="../images/favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="../images/favicon-16x16.png"/>
-        <link rel="manifest" href="../images/site.webmanifest"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png"/>
+        <link rel="manifest" href="favicon/site.webmanifest"/>
       </Helmet>
 
       {/*Header*/}
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,21 +1,28 @@
 import * as React from "react"
+import Layout from "../components/layout";
 import { StaticImage } from "gatsby-plugin-image"
 import { Link } from "gatsby"
 import {Helmet} from "react-helmet"
 
-// markup
-const NotFoundPage = () => {
-  return (
-    <section className="maintenance">
-      <Helmet defaultTitle="Jatik.id - Under maintenance"></Helmet>
+import { Navbar } from "react-bootstrap"
 
-      <div className="row">
-        <div className="col maintenance-body">
-          <StaticImage src="../images/maintenance.jpg" className="maintenance-image" alt="maintenance image"></StaticImage>
-          <Link to="/">Home</Link>
+// markup
+const NotFoundPage = ({ children }) => {
+  return (
+    <Layout>
+      <section className="maintenance">
+        <Helmet defaultTitle="Jatik.id - Under maintenance"></Helmet>
+
+        <div className="row">
+          <div className="col maintenance-body">
+            <StaticImage src="../images/maintenance.jpg" className="maintenance-image" alt="maintenance image"></StaticImage>
+            <h6>This page is under maintenance</h6>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque in viverra quisque et viverra volutpat. Tempor ultrices eleifend orci cras augue condimentum dui. Libero viverra amet bibendum congue eu fermentum. Metus, vitae at augue id etiam ultricies consequat, massa.</p>
+            <Link to="/" className="btn-solid">Back to home</Link>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </Layout>
   )
 }
 
